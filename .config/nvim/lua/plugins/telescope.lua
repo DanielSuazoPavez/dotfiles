8ae1bc748b939fd9cf32792620ab42dd5ccaaf57
Daniel Suazo Pavez <dsuazop@gmail.com>
2026-01-01 18:27:31 -0300

    feat: add telescope-ui-select extension

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,8 +1,21 @@
 return {
   "nvim-telescope/telescope.nvim",
   tag = "0.1.8",
-  dependencies = { "nvim-lua/plenary.nvim" },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope-ui-select.nvim",
+  },
   config = function()
+    local telescope = require("telescope")
+    telescope.setup({
+      extensions = {
+        ["ui-select"] = {
+          require("telescope.themes").get_dropdown(),
+        },
+      },
+    })
+    telescope.load_extension("ui-select")
+
     local builtin = require("telescope.builtin")
     vim.keymap.set("n", "<C-p>", function()
       builtin.find_files({ hidden = true, no_ignore = false, file_ignore_patterns = { "^.git/" } })
