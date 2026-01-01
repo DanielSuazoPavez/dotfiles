8a25a7825a282e9e2e6fd02b80266a2610742419
Daniel Suazo Pavez <dsuazop@gmail.com>
2026-01-01 18:23:36 -0300

    feat: add lsp for python, sql, yaml and json

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -9,16 +9,56 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls" }
-    })
+        ensure_installed = { "lua_ls", "yamlls", "jsonls" }
+      })
     end
   },
   {
     "neovim/nvim-lspconfig",
     config = function()
-      local cfg = vim.lsp.config.lua_ls
-      vim.lsp.config('lua_ls', cfg)
+      -- Enable inlay hints globally
+      vim.lsp.inlay_hint.enable(true)
+
+      -- Show diagnostics as virtual text at end of line
+      vim.diagnostic.config({
+        virtual_text = true,
+      })
+
+      -- Lua LSP
+      vim.lsp.config('lua_ls', {
+        settings = {
+          Lua = {
+            hint = {
+              enable = true,
+            },
+          },
+        },
+      })
       vim.lsp.enable('lua_ls')
+
+      -- Python LSP (basedpyright)
+      vim.lsp.config('basedpyright', {
+        settings = {
+          basedpyright = {
+            analysis = {
+              diagnosticMode = "openFilesOnly",
+              typeCheckingMode = "basic",
+              useLibraryCodeForTypes = true
+            },
+          },
+        },
+      })
+      vim.lsp.enable('basedpyright')
+
+      -- Ruff (Python linter/formatter)
+      vim.lsp.enable('ruff')
+
+      -- SQL
+      vim.lsp.enable('sqls')
+
+      -- YAML & JSON
+      vim.lsp.enable('yamlls')
+      vim.lsp.enable('jsonls')
 
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
